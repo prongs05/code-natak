834cd51ff0b11096b1aa5e093552c2f06249ff12
prongs05 <snigdhachaturv@gmail.com>
2015-06-07 20:07:07 +0530

    vacation: prototype
    To make a travel add-on app for walmart

---
+++ WalmartApp/app_html/behind_the_scene.js
@@ -0,0 +1,6 @@
+function choose() {
+	var form_inp = getElementById('question');
+	document.getElementById("demo").innerHTML = form_inp.trip_type;
+	document.getElementById("demo").innerHTML = form_inp.weather;
+	document.getElementById("demo").innerHTML = form_inp.activity;
+}
