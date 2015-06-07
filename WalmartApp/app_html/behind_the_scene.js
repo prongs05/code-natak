1b256b8245e0238f3626e1c0e16174243e9fa89b
prongs05 <snigdhachaturv@gmail.com>
2015-06-07 22:24:27 +0530

    Vacation: Change to dropdown
    Questions updated

--- WalmartApp/app_html/behind_the_scene.js
+++ WalmartApp/app_html/behind_the_scene.js
@@ -2,5 +2,5 @@ function choose() {
 	var form_inp = getElementById('question');
 	document.getElementById("demo").innerHTML = form_inp.trip_type;
 	document.getElementById("demo").innerHTML = form_inp.weather;
-	document.getElementById("demo").innerHTML = form_inp.activity;
+
 }
