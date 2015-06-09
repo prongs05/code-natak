0f1690e6db32f079ed1ff4f04284c653bea45796
Tanmay Binaykiya <binaykiya.tanmay@gmail.com>
2015-06-09 23:51:06 +0530

    API JSONP calls

--- WalmartApp/js/script.js
+++ WalmartApp/js/script.js
@@ -36,49 +36,18 @@ function choose() {
 		activity = activity+100000;
 	}
 	if (activity === 0){
-	    alert("No activity seletced");
+	    alert("No activity selected");
 	}
-
-	var url = "http://api.walmartlabs.com/v1/taxonomy?apiKey=ktntg7wnkdyg2w2ax4u8jngd&format=xml";
-	var xhr = createCORSRequest('GET', url);
-	if (!xhr) {
-	    alert('CORS not supported');
-	}
-	xhr.send();
-
-	var text = xhr.responseText;
-	alert("Response from CORS request to ", text);
-
 	console.log("place:", place);
 	console.log("activity:",activity);
-
-}
-
-
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
-
-	$.ajax({
-	  url: "http://api.walmartlabs.com/v1/taxonomy?format=json&apiKey=ktntg7wnkdyg2w2ax4u8jngd",
-	  crossDomain:true,
-	  context: document.body
-	}).done(function() {
-	  console.log("done");
-	});
 
 	// $.getJSON( "http://api.walmartlabs.com/v1/taxonomy?format=json&apiKey=ktntg7wnkdyg2w2ax4u8jngd", function( data ) {
 	// 	console.log(data);
 	// });
+
+	$.getJSON( "http://api.walmartlabs.com/v1/search?query=ipod&apiKey=ktntg7wnkdyg2w2ax4u8jngd&callback=?",function( data ) {
+		console.log(data);
+	}); 
+	// $.getJSON("http://api.walmartlabs.com/v1/taxonomy?format=json&apiKey=ktntg7wnkdyg2w2ax4u8jngd&callback=?foo");
 }
+
